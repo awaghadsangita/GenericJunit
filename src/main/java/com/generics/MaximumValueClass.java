63be9672665716251dff8dd8a4c41555f46ed600
Admin <you@example.com>
2019-11-28 16:06:51 +0530

    ADD:Given 3 Intgers find the maximum

---
+++ src/main/java/com/generics/MaximumValueClass.java
@@ -0,0 +1,13 @@
+package com.generics;
+
+public class MaximumValueClass {
+    public Integer findMaximum(Integer a, Integer b, Integer c) {
+        Integer max=a;
+        if(b.compareTo(max)>0)
+            max=b;
+        if(c.compareTo(max)>0)
+            max=c;
+
+        return max;
+    }
+}
--- src/test/java/com/generics/testCaseGenerics.java
+++ src/test/java/com/generics/testCaseGenerics.java
@@ -4,5 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testCaseGenerics {
-
+    @Test
+    public void givenThreeInteger_ShouldReturnMaximum() {
+        Integer a=100;
+        Integer b=500;
+        Integer c=700;
+        MaximumValueClass obj = new MaximumValueClass();
+        Integer result = obj.findMaximum(a, b, c);
+        Assert.assertEquals(c,result);
+    }
 }
