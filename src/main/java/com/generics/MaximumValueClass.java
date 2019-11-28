89d903c1ef6cb722d75b50c948c76e7ec7068376
Admin <you@example.com>
2019-11-28 16:18:46 +0530

    ADD:Given 3 Doubles find the Maximum

--- src/main/java/com/generics/MaximumValueClass.java
+++ src/main/java/com/generics/MaximumValueClass.java
@@ -10,4 +10,14 @@ public class MaximumValueClass {
 
         return max;
     }
+
+    public Double findMaximum(Double a, Double b, Double c) {
+        Double max=a;
+        if(b.compareTo(max)>0)
+            max=b;
+        if(c.compareTo(max)>0)
+            max=c;
+
+        return max;
+    }
 }
--- src/test/java/com/generics/testCaseGenerics.java
+++ src/test/java/com/generics/testCaseGenerics.java
@@ -10,7 +10,16 @@ public class testCaseGenerics {
         Integer b=500;
         Integer c=700;
         MaximumValueClass obj = new MaximumValueClass();
-        Integer result = obj.findMaximum(a, b, c);
+        Integer result = (Integer) obj.findMaximum(a, b, c);
+        Assert.assertEquals(c,result);
+    }
+    @Test
+    public void givenThreeFDouble_ShouldReturnMaximum() {
+        Double a=100.75;
+        Double b=500.75;
+        Double c=700.57;
+        MaximumValueClass obj = new MaximumValueClass();
+        Double result = obj.findMaximum(a, b, c);
         Assert.assertEquals(c,result);
     }
 }
