ba7329f446bb12f8c552acb533fa373c485c92bf
Admin <you@example.com>
2019-11-29 10:54:10 +0530

    ADD:Extend Max Method to take more 3 Paramters

--- src/main/java/com/generics/MaximumValueClass.java
+++ src/main/java/com/generics/MaximumValueClass.java
@@ -1,27 +1,20 @@
 package com.generics;
 
 public class MaximumValueClass<E extends Comparable<E>> {
-    E a;
-    E b;
-    E c;
-
-    public MaximumValueClass(E a, E b, E c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-    public void printMax(E max)
-    {
-        System.out.println("Maximum Number :"+max);
+    public void printMax(E max) {
+        System.out.println("Maximum Number :" + max);
     }
 
-    public E testMaximum() {
-        E max=this.a;
-        if(this.b.compareTo(max)>0)
-            max=this.b;
-        if(this.c.compareTo(max)>0)
-            max=this.c;
-        this.printMax(max);
-        return max;
+    public E testMaximum(E... items) {
+        for (int i = 0; i < items.length - 1; i++) {
+            for (int j = 0; j < items.length - i - 1; j++) {
+                if (items[j].compareTo(items[j + 1]) > 0) {
+                    E temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
+                }
+            }
+        }
+        return items[items.length - 1];
     }
 }
--- src/test/java/com/generics/testCaseGenerics.java
+++ src/test/java/com/generics/testCaseGenerics.java
@@ -6,29 +6,24 @@ import org.junit.Test;
 public class testCaseGenerics {
     @Test
     public void givenThreeInteger_ShouldReturnMaximum() {
-        Integer a=100;
-        Integer b=500;
-        Integer c=700;
-        MaximumValueClass<Integer> obj = new <Integer>MaximumValueClass(a,b,c);
-        Integer result = obj.testMaximum();
-        Assert.assertEquals(c,result);
+        Integer[] a={700,200,500,300,600};
+
+        MaximumValueClass<Integer> obj = new <Integer>MaximumValueClass();
+        Integer result = obj.testMaximum(a);
+        Assert.assertEquals(a[4],result);
     }
     @Test
     public void givenThreeFloat_ShouldReturnMaximum() {
-        Float a=100.75f;
-        Float b=500.75f;
-        Float c=700.57f;
-        MaximumValueClass<Float> obj = new<Float> MaximumValueClass(a,b,c);
-        Float result =obj.testMaximum();
-        Assert.assertEquals(c,result);
+        Float[] a={100.75f,500.75f,700.57f,50.25f,250.75f};
+        MaximumValueClass<Float> obj = new<Float> MaximumValueClass();
+        Float result =obj.testMaximum(a);
+        Assert.assertEquals(a[4],result);
     }
     @Test
     public void givenThreeStrings_ShouldReturnMaximum() {
-        String a="Kavita";
-        String b="Sneha";
-        String c="Sangita";
-        MaximumValueClass<String> obj = new<String> MaximumValueClass(a,b,c);
-        String result = obj.testMaximum();
-        Assert.assertEquals(b,result);
+        String[] a={"Kavita","Sneha","Sangita","anita","sunita"};
+        MaximumValueClass<String> obj = new<String> MaximumValueClass();
+        String result = obj.testMaximum(a);
+        Assert.assertEquals(a[4],result);
     }
 }
