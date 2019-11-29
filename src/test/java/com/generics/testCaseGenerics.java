9d81737d2647ebe4454c3d09242ff27eab59786f
Admin <you@example.com>
2019-11-29 12:24:04 +0530

    REFACTOR:Send Different Variables Instead of Array as Paramter

--- src/test/java/com/generics/testCaseGenerics.java
+++ src/test/java/com/generics/testCaseGenerics.java
@@ -6,24 +6,38 @@ import org.junit.Test;
 public class testCaseGenerics {
     @Test
     public void givenThreeInteger_ShouldReturnMaximum() {
-        Integer[] a={700,200,500,300,600};
+        Integer a=700;
+        Integer b=200;
+        Integer c=500;
+        Integer d=300;
+        Integer e=600;
 
         MaximumValueClass<Integer> obj = new <Integer>MaximumValueClass();
-        Integer result = obj.testMaximum(a);
-        Assert.assertEquals(a[4],result);
+        Integer result = obj.testMaximum(a,b,c,d,e);
+        Assert.assertEquals(a,result);
     }
     @Test
     public void givenThreeFloat_ShouldReturnMaximum() {
-        Float[] a={100.75f,500.75f,700.57f,50.25f,250.75f};
+        Float a=700.75f;
+        Float b=200.45f;
+        Float c=500.25f;
+        Float d=300.25f;
+        Float e=600.25f;
+
         MaximumValueClass<Float> obj = new<Float> MaximumValueClass();
-        Float result =obj.testMaximum(a);
-        Assert.assertEquals(a[4],result);
+        Float result =obj.testMaximum(a,b,c,d,e);
+        Assert.assertEquals(a,result);
     }
     @Test
     public void givenThreeStrings_ShouldReturnMaximum() {
-        String[] a={"Kavita","Sneha","Sangita","anita","sunita"};
+        String a="Kavita";
+        String b="Sneha";
+        String c="Sangita";
+        String d="anita";
+        String e="sunita";
+
         MaximumValueClass<String> obj = new<String> MaximumValueClass();
-        String result = obj.testMaximum(a);
-        Assert.assertEquals(a[4],result);
+        String result = obj.testMaximum(a,b,c,d,e);
+        Assert.assertEquals(e,result);
     }
 }
